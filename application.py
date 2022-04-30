94d1f6e4a9670302f4fa8196b2c0dac48fb9bd74
smorenorave <47001413+smorenorave@users.noreply.github.com>
2022-04-30 09:14:23 -0500

    Update shutdown

--- application.py
+++ application.py
@@ -54,7 +54,6 @@ application.add_url_rule('/', 'index', (lambda: header_text +
     say_hello() + instructions + footer_text))
 
 # add a rule when the page is accessed with a name appended to the site
-# URL.
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
